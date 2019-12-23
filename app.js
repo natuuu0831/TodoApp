c1e8ecbddd537fa522e2999d66f58f90a3905ff6
神長夏暉 <kaminaga@uz-inc.co.jp>
2019-12-23 19:03:35 +0900

    日付での絞込み機能(filter関数内で条件分岐させて絞り込んでいる) 削除するときダイアログを出力

--- app.js
+++ app.js
@@ -2,34 +2,32 @@ let vm = new Vue({
   el: "#app",
   data: {
     input: "",
-    filterSelected: undefined,
-    filterProgress: [
+    selectedFilteringItem: null,
+    progressLists: [
       { name: "未着手", value: "yet", done: false },
       { name: "作業中", value: "work", done: false },
       { name: "完了", value: "comp", done: false }
     ],
-    filterLimit: [
-      { name: "3日以内", value: "three", done: false },
-      { name: "その他", value: "other", done: false }
-    ],
-    uid: 0,
-    lists: [],
+    filteringLimit: false,
+    listId: 0,
+    taskLists: [],
+
+    // v-calender Data
     mode: "single",
     formats: {
       input: ["YYYY-MM-DD"]
     },
-    selectedDate: new Date(),
-    fantactic: this.listFilter
+    selectedDate: new Date()
   },
   methods: {
-    enter() {
+    addTask() {
       var text = this.input;
       if (!text) return;
-      this.lists.push({
-        id: this.uid++,
+      this.taskLists.push({
+        id: this.listId++,
         text: text,
-        select: "yet",
-        _select:"yet",
+        progress: "yet",
+        _progress: "yet",
         date: this.selectedDate,
         show: false,
         limit: this.diffDate(this.selectedDate)
@@ -37,73 +35,99 @@ let vm = new Vue({
       this.input = "";
     },
     listRemove(list) {
-      let index = this.lists.indexOf(list);
-      this.lists.splice(index, 1);
+      let index = this.taskLists.indexOf(list);
+      var result = confirm("削除してもよろしいですか？");
+      if (result) {
+        this.taskLists.splice(index, 1);
+      } else {
+        list.show = list.show;
+        return false;
+      }
+      this.taskLists.splice(index, 1);
     },
-    showModal(list) {
-      if(list.show == true){// 閉じる時
-        list.select = list._select;
+    ToggleModal(list) {
+      if (list.show == true) {
+        // 閉じる時
+        list.progress = list._progress;
         list.limit = this.diffDate(list.date);
-        list.limit < 0 ? list.limit = 0 : list.limit;
-      } 
+        list.limit < 0 ? (list.limit = 0) : list.limit;
+      }
       list.show = !list.show;
     },
-    
+
     diffDate(date2) {
       let date1 = new Date();
       let compDate = (date2 - date1) / 86400000;
       return Math.ceil(compDate);
     },
-   
-    filterSelect(filterItem) {
-      let selectValue = filterItem.value;
-      if (this.filterSelected !== selectValue) {
-        this.filterSelected = selectValue;
+
+    filterItemSelect(progressList) {
+      let sFI = this.selectedFilteringItem;
+      let selectValue = progressList.value;
+
+      if (sFI !== selectValue) {
+        sFI = selectValue;
       } else {
-        this.filterSelected = null;
+        sFI = null;
       }
-      let count = 0;
-      let f = this.filterProgress;
-      for (let i in f) {
-        f[i].done == false&&count++;
-        if (count !== f.length) {
-          f[i].done = false;
-          filterItem.done = true;
-          if (this.filterSelected == null) {
-            filterItem.done = false;
+      let noSelectedFilterItem = 0;
+      let l = this.progressLists;
+      for (let i in l) {
+        if (l[i].done == false) {
+          noSelectedFilterItem++;
+        }
+        if (noSelectedFilterItem !== l.length) {
+          l[i].done = false;
+          progressList.done = true;
+          if (sFI == null) {
+            progressList.done = false;
           }
         }
       }
+    },
+    filterLimit() {
+      this.filteringLimit = !this.filteringLimit;
     }
   },
   computed: {
     listCount() {
-      return this.lists.length;
+      return this.taskLists.length;
     },
-    listFilter() {
-      return this.lists.filter(function(el) {
-        if (this.filterSelected) {
-          return el.select == this.filterSelected;
+    ｆilteredlist() {
+      let sFI = this.selectedFilteringItem;
+      let fL = this.filteringLimit;
+      return this.taskLists.filter(function(el) {
+        if (sFI && !fL) {
+          return el.progress == sFI;
+        } else if (fL) {
+          if (sFI) {
+            return el.progress == sFI && el.limit <= 3;
+          } else if (!sFI) {
+            return el.limit <= 3;
+          }
         } else {
-          return this.lists;
+          return this.taskLists;
         }
       }, this);
     },
-    listFilting() {
-      return this.listFilter.length;
+    ｆilteredlistCount() {
+      return this.ｆilteredlist.length;
     }
   },
   filters: {
     customformat: function(value) {
       return moment(value).format("YYYY/MM/DD");
     },
-    changeselect(value) {
-      if (value == "comp") {
-        return "完了";
-      } else if (value == "work") {
-        return "作業中";
-      } else {
-        return "未着手";
+    changeProgressLang(value) {
+      switch (value) {
+        case "comp":
+          return "完了";
+        case "work":
+          return "作業中";
+        case "yet":
+          return "未着手";
+        default:
+          break;
       }
     }
   }
